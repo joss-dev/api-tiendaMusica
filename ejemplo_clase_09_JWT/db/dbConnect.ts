d77e5855fef167208174e1f28f2d9fb4e8b317fa
AgustinsantHST <agustin.santillan@hstpathways.com>
2024-08-09 12:15:23 -0300

    Add class 09 code

--- ejemplo_clase_09_JWT/db/dbConnect.ts
+++ ejemplo_clase_09_JWT/db/dbConnect.ts
@@ -7,6 +7,7 @@ export default async function dbConnect() {
   if (mongoose.connection.readyState >= 1) return;
   try {
     const dbURI = process.env.DB_URI!;
+
     await mongoose.connect(dbURI);
     console.log("Database connected");
   } catch (error) {
